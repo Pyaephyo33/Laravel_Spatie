679c0cdca0ec7c23eee5b914f7c49f4c5311abb4
Pyaephyo33 <pyaephyolinn778899@gmail.com>
2023-11-13 22:58:37 +0630

    first commit

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run():void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+        ])->assignRole('writer', 'admin');
+    }
+}
