df23c57525b597080eaa302f344248d064398c44
Pyaephyo33 <pyaephyolinn778899@gmail.com>
2023-11-20 00:18:18 +0630

    ppl

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,10 +2,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                    <br>
-                    hola, this is admin
+                <div class="p-6 bg-white border-b border-gray-200">
+                   Admin Content
                 </div>
             </div>
         </div>
